83968ebe387838b8d317279887dd51a7f2b23b69
Axel Alfredo Sanchez Ortega <proaxel1214@gmail.com>
2024-09-17 21:05:08 -0600

    primer commit martes 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
 
+//Decorador
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  selector: 'app-root', 
+  
+  templateUrl: './app.component.html',  //referencia html que contienen el componente
+  styleUrl: './app.component.css' //estilo que pertenece el componente
 })
+
+//Typescript
 export class AppComponent {
   title = 'basesAngular';
+//funciones
+  duplicarNumero(x:number):number{
+    return x*2;
+  }
+//objetos
+alumno={
+  matricula:1234,
+  nombre:'Axel',
+  fechaInscrito:new Date(),
+  pago:2220
+};
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+//Hacer referencia a componentes
+
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product/product-list/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule,
